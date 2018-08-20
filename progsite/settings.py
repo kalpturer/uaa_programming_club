b0f9b342f3dbc485118723eac300c8e879f755b5
Kaya Alpturer <kalpturer@gmail.com>
2018-08-20 22:24:34 +0300

    Ready for deployment

--- progsite/settings.py
+++ progsite/settings.py
@@ -132,7 +132,7 @@ LOGOUT_REDIRECT_URL = '/'
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASE['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
